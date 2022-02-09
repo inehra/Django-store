7e2b60c27991f646244d7e9bd42612df48463889
inehra <neharkar2101999@gmail.com>
2022-02-09 20:43:47 +0530

    'django-store-start'

---
+++ store_project/urls.py
@@ -0,0 +1,11 @@
+# Admin URLs
+
+import debug_toolbar
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include('playground.urls')),
+    path('__debug__', include(debug_toolbar.urls))
+]
